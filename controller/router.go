ef0c8eb11ac87fd0cea9a4ad9e87cd36e0d39932
IvanS <songchunxia@qq.com>
2023-03-17 23:56:59 +0800

    Initial commit

---
+++ controller/router.go
@@ -0,0 +1,22 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//Router 实例化router对象，可使用该对象点出首字母大写的方法(跨包调用)
+var Router router
+
+//定义router结构体
+type router struct{}
+
+//InitApiRouter 初始化路由，创建测试api接口
+func (*router) InitApiRouter(r *gin.Engine) {
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg":  "testApi success!",
+			"data": nil,
+		})
+	})
+}
