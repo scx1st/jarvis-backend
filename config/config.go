605884f7d474f8b8663b0058bd0e64309be0e037
IvanS <songchunxia@qq.com>
2023-03-23 10:26:44 +0800

    初始化k8s client

--- config/config.go
+++ config/config.go
@@ -2,4 +2,6 @@ package config
 
 const (
 	ListenAddr = "0.0.0.0:9090"
+	//KubeConfigs 一个config，为了验证多集群用了两个一样的
+	KubeConfigs = `{"Cluster-1":"F:\\client-config\\config","Cluster-2":"F:\\client-config\\config"}`
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wonderivan/logger"
 	"jarvis-backend/config"
 	"jarvis-backend/controller"
+	"jarvis-backend/service"
 	"net/http"
 	"os"
 	"os/signal"
@@ -15,6 +16,8 @@ import (
 func main() {
 	//初始化gin对象
 	r := gin.Default()
+	//初始化 k8s client
+	service.K8s.Init()
 	// 初始化路由规则
 	controller.Router.InitApiRouter(r)
 	//gin server启动
@@ -33,13 +36,6 @@ func main() {
 	quit := make(chan os.Signal)
 	//将操作系统中的中断信号(os.Interrupt)转化为 quit 通道中的消息，以便在程序运行时能够捕获到该信号并做出相应的处理。
 	signal.Notify(quit, os.Interrupt)
-	//程序会一直阻塞在此处，直到信号通道 quit 中有数据传入，
-	//即接收到操作系统中断信号(os.Interrupt)，
-	//此时程序会从该阻塞状态中恢复执行，
-	//并继续向下执行程序中的后续代码，完成程序的退出操作
-	//这种方式保证了程序可以在接收到中断信号时优雅地退出，
-	//而不是突然终止，
-	//避免了可能产生的资源泄露等问题
 	<-quit
 	//设置ctx超时时间
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
